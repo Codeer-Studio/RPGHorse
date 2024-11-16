450ea0d0536a02ef450ccf81a9ac6230278841ca
Thomas Froud <thomasfroud10@gmail.com>
2024-11-16 14:09:35 +0000

    Initial project creation

---
+++ src/main/java/io/github/CodeerStudio/RPGHorse/RPGHorse.java
@@ -0,0 +1,17 @@
+package io.github.CodeerStudio.RPGHorse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RPGHorse extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
