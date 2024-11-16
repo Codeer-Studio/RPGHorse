f41a0dd3d8170064b71e15ae98c5eaf4d5e489f0
Thomas Froud <thomasfroud10@gmail.com>
2024-11-16 14:22:48 +0000

    Setup jar output
    The project now creates a jar file when built to be used and tested by a server

--- src/main/java/io/github/CodeerStudio/RPGHorse/RPGHorse.java
+++ src/main/java/io/github/CodeerStudio/RPGHorse/RPGHorse.java
@@ -1,5 +1,6 @@
 package io.github.CodeerStudio.RPGHorse;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RPGHorse extends JavaPlugin {
@@ -7,11 +8,12 @@ public final class RPGHorse extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Bukkit.getLogger().info("Hello world!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getLogger().info("Shutting down!");
     }
 }
